1fe824552edb0f787226a1171f892c6bb04d42d0
Nadeen Noshy <nadeennoshy3@gmail.com>
2024-08-03 21:30:50 +0300

    create floatingActionButton

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_notes_app/constants.dart';
 import 'package:simple_notes_app/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -6,8 +7,21 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: NotesViewBody(),
+    return Scaffold(
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 50, right: 8.0),
+        child: FloatingActionButton(
+          backgroundColor: kPrimaryColor,
+          shape: const CircleBorder(),
+          onPressed: () {},
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 30,
+          ),
+        ),
+      ),
+      body: const NotesViewBody(),
     );
   }
 }
