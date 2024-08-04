f9544377d9fa88c552898caa992b09b7eeacf953
Nadeen Noshy <nadeennoshy3@gmail.com>
2024-08-04 03:34:51 +0300

    create note modal and add packages

---
+++ lib/models/note_model.dart
@@ -0,0 +1,7 @@
+class NoteModel{
+  String title;
+  String subTitle;
+  int color;
+  String date;
+  NoteModel({required this.title,required this.subTitle,required this.color,required this.date});
+}
